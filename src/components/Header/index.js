38fc6293d30901fcec8e68846aa4b44c9f4a03d8
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-03-04 10:54:37 +0000

    Finished off bootstrap navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,36 +1,33 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Nav, Navbar} from 'react-bootstrap';
+import { NavLink, Link } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
 import logoSrc from '../../assets/images/logo.png'
 function Header() {
     return (
         <header>
-             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="p-3">
-                <NavLink className="navbar-brand" to="/" end>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="p-3">
+                <Navbar.Brand as={Link} to="/">
                     <img src={logoSrc} alt="Gurdeep Bangar logo image" className="logo" />
-                </NavLink>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id ="responsive-navbar-nav">
-                    <Nav.Link as={NavLink} to="/">
-                        New
-
-                    </Nav.Link>
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/projects" end>Projects</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/about" end>About Me</NavLink>
-                        </li>                        
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/contact" end>Contact me</NavLink>
-                        </li>
-                    </ul>
-
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav>
+                        <Nav.Item>
+                            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={NavLink} to="/projects">Projects</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={NavLink} to="/about">About Me</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
                 </Navbar.Collapse>
             </Navbar>
         </header>
     )
 }
-
 export default Header;
